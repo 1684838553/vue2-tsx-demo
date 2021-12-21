318b21605a9361cdd9dfa07018cae2724e050176
drunk喵咪 <1684838553@qq.com>
2021-12-21 11:30:23 +0800

    Create interface.ts

---
+++ src/types/interface.ts
@@ -0,0 +1,12 @@
+export type LoginType = 'account'| 'phone'
+
+export type IloginForm = {
+    username:string
+    password:string
+}
+
+export type IRoute = {
+    icon?:string,
+    show:boolean,
+    
+}
