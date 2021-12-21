2c69e88eec2fd833ac2c54476f135c9f7528906f
drunk喵咪 <1684838553@qq.com>
2021-12-21 17:16:44 +0800

    Update home.ts

--- src/mock/home.ts
+++ src/mock/home.ts
@@ -1,11 +1,34 @@
 let Mock = require('mockjs')
+const Random = Mock.Random
+
 Mock.setup({
     timeout:500 // 模拟接口延时
 })
 
-Mock.mock('http://projectList','get',{
-    'projectList|6':[{
-        'key|+1':1,
-        'title':'hello'
-    }]
-})
+const projectList = function(){
+    let list = []
+    let arr = []
+    const nameList:Array<string> = ['Alipay','Facebook','Wechat','Zhihu','Ant Design','Github']
+    for(let i =0;i<6;i++){
+        let newdata = {
+            key:i,
+            title:nameList[i],
+            description:Random.csentence(8,30),
+            date:Random.date(),
+            author:Random.cname() 
+        }
+        let newArr = {
+            author:Random.cname(),
+            description:Random.csentence(8,30),
+        }
+        list.push(newdata)
+        arr.push(newArr)
+    }
+
+    return {
+        projectList:list,
+        sourseData:arr
+    }
+}
+
+Mock.mock('http://projectList','get',projectList)
